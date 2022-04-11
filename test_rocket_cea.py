5d9425314e0fd5fbb0173db49b80ba4eda902c4a
Trenton Charlson <charlsontrent@gmail.com>
2022-04-11 00:39:17 -0400

    working python CEA wrapper code & baseline trajectory sim

---
+++ test_rocket_cea.py
@@ -0,0 +1,24 @@
+import numpy as np
+from rocketcea.cea_obj import CEA_Obj
+import pandas as pd
+import matplotlib.pyplot as plt
+
+fac_CR = 4.0
+eps = 5
+
+C = CEA_Obj( oxName='LOX', fuelName='ETHANOL', fac_CR=fac_CR)
+mr = np.linspace(1.0,3.5,num=20) #select 10 MR points
+pc = np.linspace(250,500,num=20) #select 10 chamber pressure points
+CEA_out_df = pd.DataFrame(index=mr)
+
+for mr in mr:
+    CEA_out_df.at[mr,'TAdiabatic'] = C.get_Tcomb(Pc=500, MR=mr)
+    CEA_out_df.at[mr,'ISP_sl'] = 5 #
+    x = C.estimate_Ambient_Isp(Pc=500, MR=mr, eps=eps)
+    print(x)
+    print(type(x))
+    #print(mr, C.estimate_Ambient_Isp(Pc=500.0, MR=mr, eps=5))
+
+print(CEA_out_df)
+plt.plot(CEA_out_df)
+plt.show()
